49f1df471d85238ca85055a1cd987f742a6ffb67
sahil <sahil27raj@gmail.com>
2024-11-24 01:03:28 +0530

    add the practice project

---
+++ src/app/development/analytics/page.tsx
@@ -0,0 +1,9 @@
+export default function SettingsPage() {
+    return (
+      <div>
+        <h1>Dashboard Settings</h1>
+        <p>Manage your settings here. Analytics</p>
+      </div>
+    );
+  }
+  
